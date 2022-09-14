c1345f8c7fbc058d6e96f98b4a7b1afc1a6641dd
W0688022 <evan.schubert@selu.edu>
2022-09-13 22:21:52 -0500

    Styling Update
    This was more difficult than it shouldve been

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,205 +1,68 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/test2.css'
 
 export default function Home() {
   return (
     
-    <div className="container">
-      <Head>
-        <title>VenerationSpace</title>
+    <div className="container" style={{ background: '#C1C6E4'}}>
+      <Head style={{ background: '#BEC7E4'}}>
+        <title>Veneration Space</title>
         <link rel="icon" href="/cross.ico" />
       </Head>
-
-      <header className="topnav">
-            <a href="/" className="firstnav">Home</a>
-            <a href="/statpages/about">About Us</a>
-            <a href="/statpages/apply">Apply</a>
-            <a href="/statpages/login" className="lastnav">Login</a>
-      </header>
-      <main>
-        <h1 className="title">
-          Veneration Space
-        </h1>
-
-        <div className="split">
-          <div>
-            <ul className="listButtons">
-                <li>
-                  <a href="/statpages/search" className="card">Search</a>
-                </li>
-                <li>
-                  <a href="/statpages/example" className="card">Create</a>
-                </li>
-            </ul>
+      <div class="ui menu" style={{ background: '#547FE8', color: 'white'}}>
+        <div class="ui large header item" >
+          <a href="/" class="ui item" style={{ color: 'white'}}>
+            Home
+          </a>
+          <a href="/statpages/about" class="ui item" style={{ color: 'white'}}>
+            About Us
+          </a>
+          <a href="/statpages/apply" class="ui item" style={{ color: 'white'}}>
+            Apply
+          </a>
+          <a href="/statpages/login" class="ui item" style={{ color: 'white'}}>
+            Login
+          </a>
+          <a href="/statpages/search" class="ui item" style={{ color: 'white'}}>
+            Search
+          </a>
+          <a href="/statpages/example" class="ui item" style={{ color: 'white'}}>
+            Create
+          </a>
+        </div>
+      </div>
+      <div class = "ui equal width middle aligned center aligned grid" style={{ background: '#C1C6E4'}} >
+        <div class="column six wide middle aligned grid" >
+          <div class="row">
+            <a href="/statpages/search">
+              <button class = "ui color1 button" style={{ fontSize: '4.5rem', margin:'5rem auto'}}>
+                Search
+              </button>
+            </a>
           </div>
-          
-          <div>
-          <img src="https://img3.stockfresh.com/files/s/spectral/m/27/1729712_stock-photo-cartoon-angel.jpg" alt="image placeholder" className="image"/>
+          <div class="row">
+          <a href="/statpages/example">
+              <button class = "ui color1 button" style={{ fontSize: '4.5rem', margin:'5rem auto'}}>
+                Create
+              </button>
+              </a>
           </div>
         </div>
-      </main>
-
-      {/* CSS Stuff */}
-      {/* Main color:   #736b66 */}
-      {/* Light accent: #D39130 */}
-      {/* Dark accent:  #9E4943 */}
-      {/* Light shade:  #EFF2EC */}
-      {/* Dark shade:   ##d3a67a */}
-      <style jsx>{`
-        .container {
-          background-color: #d3a67a;
-          height: 800px;
-        }
-
-          main {
-            font-family: monaco, Consolas, "Lucida Console", monospace;
-          }
-        
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color:#736b66;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 2.5% 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          filter: drop-shadow(0 4px 3px rgba(0, 0, 0, 0.6));
-          -webkit-text-stroke: 1px black;
-          text-align: center;
-          color: #f2f2f2;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #d3a67a;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-        .listButtons{
-          margin-top:-25%;
-        }
-
-        .listButtons li {
-          display: block;
-          margin: 50% 0;
-
-        }
-        .split{
-          display: flex;
-        }
-        .split div{
-          margin: 0 auto;
-        }
-        .image{
-          filter: drop-shadow(0 10px 10px rgba(0, 0, 0, 0.4));
-          height: 65%;
-          margin-right: -10rem;
-        }
-        .card {
-          filter: drop-shadow(0 10px 10px rgba(0, 0, 0, 0.4));
-          margin: 1rem;
-          padding: 2rem 20%;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 5px;
-          border-color:#2e2c2b;
-          background-color: #d1694b;
-          color:#2e2c2b;
-          font-size: 70px;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          background-color:#9E4943;
-        }
-
-        .logo {
-          height: 1em;
-        }
-        .topnav {
-          filter: drop-shadow(0 10px 10px rgba(0, 0, 0, 0.7));
-          font-family: monaco, Consolas, "Lucida Console", monospace;
-          overflow: hidden;
-          background-color: #5d534d;
-        }
-        
-        .topnav a {
-          width: 25%;
-          float: left;
-          color: #f2f2f2;
-          text-align: center;
-          padding: 14px 16px;
-          text-decoration: none;
-          font-size: 24px;
-        }
-        
-        .topnav a:hover {
-          background-color: #302d2b;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        <div class="column ten wide">
+          <div class="ui large image">
+            <img src="https://img3.stockfresh.com/files/s/spectral/m/27/1729712_stock-photo-cartoon-angel.jpg" alt="image placeholder"/>
+          </div>
+        </div>
+      </div>
+        <style jsx>{`
+            .container{
+                min-height: 100vh;
+            }
+            `}
+        </style>
     </div>
   )
 }
---
+++ nextjs-blog/testbutton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Divider, Grid, Image, Segment, Button } from 'semantic-ui-react'
+
+const ButtonTest = () => {
+    return ( 
+        <Button primary>test</Button>
+    );
+}
+
+export default ButtonTest;

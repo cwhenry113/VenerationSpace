6f4df5f7b784fe7130ab535d3af665c7981a9cef
danielamurillo <49042607+danielamurillo@users.noreply.github.com>
2022-09-28 22:39:17 -0500

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,9 +1,9 @@
 import 'semantic-ui-css/semantic.min.css'
 import 'semantic-ui-css/test2.css'
+import React from 'react'
 import {
   Container,
   Divider,
-  Dropdown,
   Grid,
   Header,
   Image,
@@ -13,7 +13,94 @@ import {
   Button,
 } from 'semantic-ui-react'
 
+const FixedMenuLayout = () => (
+  <div>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+          Veneration Space
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+        <Menu.Item as='a'>About</Menu.Item>
+        <Menu.Item as='a'>Apply</Menu.Item>
+        <Menu.Item as='a'>Login</Menu.Item>
+      </Container>
+    </Menu>
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+      
+
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h1'>Apply for a free online memorial for your loved one</Header>
+      <p>This is a basic fixed menu template using fixed size containers. we can add some sutext here to add detail.</p>
+      
+      <div>
+    <Button size='massive'>Apply</Button>
+    <Button size='massive'>Learn More</Button>
+    <Button size='massive'>Search</Button>
+  </div>
+
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+    </Container>
+
+    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+      <Container textAlign='center'>
+        <Grid divided inverted stackable>
+          <Grid.Column width={3}>
+            <Header inverted as='h4' content='Group 1' />
+            <List link inverted>
+              <List.Item as='a'>Link One</List.Item>
+              <List.Item as='a'>Link Two</List.Item>
+              <List.Item as='a'>Link Three</List.Item>
+              <List.Item as='a'>Link Four</List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Header inverted as='h4' content='Group 2' />
+            <List link inverted>
+              <List.Item as='a'>Link One</List.Item>
+              <List.Item as='a'>Link Two</List.Item>
+              <List.Item as='a'>Link Three</List.Item>
+              <List.Item as='a'>Link Four</List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Header inverted as='h4' content='Group 3' />
+            <List link inverted>
+              <List.Item as='a'>Link One</List.Item>
+              <List.Item as='a'>Link Two</List.Item>
+              <List.Item as='a'>Link Three</List.Item>
+              <List.Item as='a'>Link Four</List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={7}>
+            <Header inverted as='h4' content='Footer Header' />
+            <p>
+              Extra space for a call to action inside the footer that could help re-engage users.
+            </p>
+          </Grid.Column>
+        </Grid>
+
+        <Divider inverted section />
+        <Image centered size='mini' src='/logo.png' />
+        <List horizontal inverted divided link size='small'>
+          <List.Item as='a' href='#'>
+            Site Map
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Contact Us
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Terms and Conditions
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Privacy Policy
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
+  </div>
+)
+
+export default FixedMenuLayout

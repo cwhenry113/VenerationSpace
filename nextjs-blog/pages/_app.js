77d196e156eb63be4791da55a8360035054bddbc
danielamurillo <49042607+danielamurillo@users.noreply.github.com>
2022-09-28 21:59:19 -0500

    Update _app.js

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,19 @@
 import 'semantic-ui-css/semantic.min.css'
 import 'semantic-ui-css/test2.css'
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Button,
+} from 'semantic-ui-react'
+
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

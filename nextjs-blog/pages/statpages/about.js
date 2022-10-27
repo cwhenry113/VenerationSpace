a5ec32486973631889f7d49627a09a930d76d513
jwselu <joel.white@selu.edu>
2022-10-27 15:33:11 -0500

    About Page Update
    Updated about page wording

--- nextjs-blog/pages/statpages/about.js
+++ nextjs-blog/pages/statpages/about.js
@@ -18,7 +18,11 @@ export default function About(){
         <div class="header">
           About Veneration Space
         </div>
-        <p><br/>The Veneration Space is a website used to honor the dead by creating a tribute webpage for them. The webpages include anything from information about them, testimonials from their friends and families, and stories about their life. <br/><br/>Pages on our website can have information be requested added, deleted or changed in order to better suit who the memorial is of. Comments can also be made in order to talk about them or with people they knew. <br/><br/>Moderation is carried out through Guardians. Guardians are relatives or close friends that apply to help make sure there is peace on the webpage of the person.</p>
+        <p><br/>Veneration Space is a website used to honor the dead by creating a tribute webpage for them. 
+        These webpages, called "Venerations", contain lots of information on the deceased, including testimonials from their friends and family, and stories of their life. 
+        <br/><br/>Venerations can have their information added, deleted or updated in order to better represent the deceased's life. 
+        Additionally, comments can be made on a Veneration, so that loved ones may share what the deceased meant to them. <br/><br/>Moderation is carried out through Guardians. 
+        Guardians are relatives or close friends that apply to help ensure there is peace on the Veneration page of the ones they care about.</p>
       </div>
     </div>
       <style jsx>{`

989e9a9fb3dcb66112b38cc32272296bbe467141
danielamurillo <49042607+danielamurillo@users.noreply.github.com>
2022-11-03 15:08:44 -0500

    Update SubmittedForm.js

--- nextjs-blog/components/SubmittedForm.js
+++ nextjs-blog/components/SubmittedForm.js
@@ -13,16 +13,16 @@ import { NavBar } from './components/NavBar';
     <NavBar />
     <Container text style={{ marginTop: '6em' }}>
   
-    <Grid columns={2} relaxed='very'>
+    
       <Grid.Column>
       
-      <h2 style={{textAlign: "center"}}>Thank you for creating a Veneration!</h2>
+      <h2 style={{textAlign: "center"}}>Thank you for submitting an appliciation to create a veneration!!</h2>
       <p text style={{ marginTop: '6em' }}>
       Your Veneration will be created shortly. A copy of your application will be sent to the email that was submitted. 
     </p>
 
        </Grid.Column>
-    </Grid>
+   
     
     </Container>
 

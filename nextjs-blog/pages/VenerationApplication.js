27d310253a4ce95f9566052e7db027bc26c6dda0
danielamurillo <49042607+danielamurillo@users.noreply.github.com>
2022-11-03 14:28:23 -0500

    Create VenerationApplication.js
    file is able to collect and record data through google forms. and view responses through a spreadsheet.  need help routing create button to form and create/link thanks for submitting form

---
+++ nextjs-blog/pages/VenerationApplication.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+  Container,
+  Grid,
+} from 'semantic-ui-react'
+import { NavBar } from './components/NavBar';
+
+
+
+  export const VenerationApplication = () => {
+      return (
+  <div>
+    <NavBar />
+        
+    <Container text style={{ marginTop: '6em' }}>  
+        
+    <Grid columns={2} relaxed='very'>
+      <Grid.Column>
+      
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf6rhkqn_sy0R8FSweQtVNXPGFoJJg2hrXHF9l6xxN9USgpEg/viewform?embedded=true" width="640" height="1455" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+   
+    </Grid.Column>
+     </Grid>
+    
+    </Container>
+
+    <div class="ui divider"></div>
+    <div class="ui tabular menu"><a class="active item">Bio</a><a class="item">Photos</a></div>
+
+  </div>
+      );
+    };

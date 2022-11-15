58955819d7a5fb79d959b7c77c9ee0ae5a4b0d1b
W0688022 <69818417+W0688022@users.noreply.github.com>
2022-11-14 18:18:59 -0600

    Add files via upload

--- nextjs-blog/pages/api/veneration/findById.js
+++ nextjs-blog/pages/api/veneration/findById.js
@@ -6,40 +6,18 @@ Request body example:
 */
 
 import connectMongo from '../../../utils/connectMongo';
-import Comment from '../../../models/comment';
+import Veneration from '../../../models/venModel';
 
 export default async function findVeneration(req, res) {
     try {
         console.log('CONNECTING TO MONGO');
         await connectMongo();
         console.log('CONNECTED TO MONGO');
-
-        console.log('FINDING DOCUMENT');
-        const foundCom = await Comment.findById(req.body).exec();
-        res.json({ foundCom });
+        const foundVen = await Veneration.find(req.body);
+        res.json({ foundVen });
+        console.log(foundVen);
     } catch (error) {
         console.log(error);
         res.json({ error });
     }
 }
-
-/*
-BOILERPLATE FUNCTION
- const findCommentById = async () => {
-    console.log('FINDING COMMENT BY ID');
-    //!!!THIS WILL ALWAYS DO THE SAME THING. THIS IS BOILERPLATE. FILL IN OR REMOVE THESE FIELDS USING THE FRONTEND.!!!
-    const id = "6351d6871586cb9e8425b18c"
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    const res = await fetch('/api/comment/findById', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        _id: `${id}`,
-      }),
-    });
-    const data = await res.json();
-    console.log(data);
-  };
-*/

ab23747883ac2125391dcded710cab9492983a16
jwselu <joel.white@selu.edu>
2022-11-17 23:40:43 -0600

    Reverted normal example routing
    Changed back to the routes I created. Sorry I couldn't figure out a different way

--- nextjs-blog/pages/statpages/example2.js
+++ nextjs-blog/pages/statpages/example2.js
@@ -1,46 +1,60 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import axios from 'axios';
 import {React, useEffect, useState} from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import 'semantic-ui-css/test2.css'
 import {Container, Image, Grid} from 'semantic-ui-react'
 import { NavBarOther } from '/components/NavBarOther';
 import { Search } from '/components/Search';
-const src1 = 'https://images.unsplash.com/photo-1620506117452-99cd6b6dcd3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
 export default function About(){ 
-  /*
-------- I've tried with this code -------
-
-  const id = "6351d6871586cb9e8425b18c"
-  let [name, setName] = useState(null)
-  useEffect(() => {findVenerationById();}, [])
-  const findVenerationById = async () => {
-    fetch('/api/veneration/findById', {_id: `${id}`})
-    .then(response => response.json())
-    .then(data => setName(data.givenName))
-  };*/
-
-
-
-  const convertedId = '6351d6871586cb9e8425b18c'
-  const [name, setName] = useState('')
-  const findVenerationById = async () => {
-    const id = "6351d6871586cb9e8425b18c"
-    const res = await fetch('/api/veneration/findById', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        _id: `${id}`
-      }),
-    });
-    const data = await res.json();
-    console.log(data);
-  };
-
-
-  findVenerationById();
+  var personId = "637715ac6e775a99f72685b9"
+  //Gets person's photo
+  const [pic, setPic] = useState('')
+  useEffect(() => {getPic();}, [])
+  const getPic = () => {
+    axios.get('http://localhost:3000/api/getOne/'+ personId)
+    .then(res => {
+      setPic(res.data.pictureURL.toString())
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+    //Gets person's name
+    const [name, setName] = useState('')
+    useEffect(() => {getName();}, [])
+    const getName = () => {
+      axios.get('http://localhost:3000/api/getOne/'+ personId)
+      .then(res => {
+        console.log(res.data.name)
+        setName(res.data.firstName + " " + res.data.middleName + " " + res.data.lastName)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+    //Gets person's birth and death dates
+    const [dates, setDates] = useState('')
+    useEffect(() => {getDate();}, [])
+    const getDate = () => {
+      axios.get('http://localhost:3000/api/getOne/' + personId)
+      .then(res => {
+        setDates(res.data.birthDate + "-" + res.data.deathDate)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+    //Gets person's bio
+    const [bio, setBio] = useState('')
+    useEffect(() => {getBio();}, [])
+    const getBio = () => {
+      axios.get('http://localhost:3000/api/getOne/' + personId)
+      .then(res => {
+        console.log(res.data.bio)
+        setBio(res.data.bio)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
     return(
       <div className="container" style={{ backgroundImage: "url(/bioPictures/background.jpg)", backgroundSize: 'cover'}}>
       <NavBarOther />
@@ -52,10 +66,10 @@ export default function About(){
               Options
             </button>
           </Grid.Column>
-      <h1 style={{textAlign: "center", fontSize:"3vw", fontFamily:"MrsEaves-Italic"}}></h1>
-      <h3 style={{textAlign: "center", fontSize:"1.5vw"}}>Dates</h3>
-      <Image src={src1} size='medium' centered />
-        <p text style={{ marginTop: '2em', fontSize:"1.5vw", fontFamily:"MrsEaves-Italic"}}>Bio</p>
+      <h1 style={{textAlign: "center", fontSize:"3vw", fontFamily:"MrsEaves-Italic"}}>{name}</h1>
+      <h3 style={{textAlign: "center", fontSize:"1.5vw"}}>{dates}</h3>
+      <Image src={pic} size='medium' centered />
+        <p text style={{ marginTop: '2em', fontSize:"1.5vw", fontFamily:"MrsEaves-Italic"}}>{bio}</p>
       <h3 style={{textAlign: "center"}}>Add Memory
       <form>
         <input align="right" type="text" id="comment" name="comment"/>
